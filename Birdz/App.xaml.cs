f6eb81d84688b8137f58caa6a3857024b4d7ab36
snacks9405 <rodriguezal.525@gmail.com>
2022-11-02 11:54:11 -0500

    initial code dump. branches from here please

---
+++ Birdz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Birdz;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Birdz/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Birdz;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
