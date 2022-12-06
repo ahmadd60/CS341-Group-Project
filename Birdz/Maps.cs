8129dfb9895b024747fa6c93a707f41f13eece9f
snacks9405 <93486382+snacks9405@users.noreply.github.com>
2022-12-06 01:44:37 -0600

    Permissions
    added maps permissions and started working on finalizing the entry display page

--- Birdz/Maps.cs
+++ Birdz/Maps.cs
@@ -4,6 +4,43 @@ namespace Birdz
 {
     class Maps
     {
+        private static CancellationTokenSource _cancelTokenSource;
+        private static bool _isCheckingLocation;
 
+        public async Task GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                        Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+            }
+            // Catch one of the following exceptions:
+            //   FeatureNotSupportedException
+            //   FeatureNotEnabledException
+            //   PermissionException
+            catch (Exception ex)
+            {
+                CancelRequest();
+
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
     }
 }
